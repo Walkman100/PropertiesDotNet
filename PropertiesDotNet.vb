07247abd3a75e7f48faf2369605fe87bbe94ae5b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-06-29 01:41:44 +0200

    Added basic code

--- PropertiesDotNet.vb
+++ PropertiesDotNet.vb
@@ -1,3 +1,46 @@
-﻿Public Class PropertiesDotNet
+﻿Imports System.IO
+Imports System.IO.File
+
+Public Class PropertiesDotNet
+    'File Name, rename button, copy file name
+    'file path, copy file path, copy full file path
+    'icon: picturebox
+    'date whatevered
+    'sizes
+    'attributes
+    'default open with?
+    'set default open with
+    'launch
+    'launch with [..]
+    'ok, cancel, apply
     
+    Sub PropertiesDotNet_Load(sender As Object, e As EventArgs) Handles Me.Load
+        For Each s As String In My.Application.CommandLineArgs
+            lblLocation.Text &= s
+        Next
+        CheckData()
+    End Sub
+    
+    Sub CheckData
+        chkHidden.Checked = GetAttributes(lblLocation.Text).HasFlag(FileAttributes.Hidden)
+        chkSystem.Checked = GetAttributes(lblLocation.Text).HasFlag(FileAttributes.System)
+    End Sub
+    
+    Sub chkHidden_CheckedChanged() Handles chkHidden.Click
+        If chkHidden.Checked Then
+            SetAttributes(lblLocation.Text, FileAttribute.Hidden)
+        Else
+            SetAttributes(lblLocation.Text, FileAttributes.Normal)
+        End If
+        CheckData()
+    End Sub
+    
+    Sub chkSystem_CheckedChanged() Handles chkSystem.Click
+        If chkSystem.Checked Then
+            SetAttributes(lblLocation.Text, FileAttribute.System)
+        Else
+            SetAttributes(lblLocation.Text, FileAttribute.Normal)
+        End If
+        CheckData()
+    End Sub
 End Class
