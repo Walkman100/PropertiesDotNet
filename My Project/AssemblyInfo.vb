ea03bb0f4b2b1381a419213666874d2dc84a710c
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-08-24 03:32:22 +0200

    Updated AssemblyInfo

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-Imports System
+﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("A properties window made in VB.Net")> 
 <Assembly: AssemblyCompany("WalkmanOSS")> 
 <Assembly: AssemblyProduct("PropertiesDotNet")> 
-<Assembly: AssemblyCopyright("FOSS from Walkman 2015")> 
+<Assembly: AssemblyCopyright("FOSS from Walkman 2015-17")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
