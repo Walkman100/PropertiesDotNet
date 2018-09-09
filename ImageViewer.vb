c77d2c1eea06834fbe7738fe551c13745ae66907
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2018-09-09 15:51:50 +0200

    added close button to ImageViewer to enable ESC support

--- ImageViewer.vb
+++ ImageViewer.vb
@@ -13,6 +13,7 @@ Public Class ImageViewer
     Private Sub InitializeComponent()
         Me.fileImage = New System.Windows.Forms.PictureBox()
         Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
         CType(Me.fileImage,System.ComponentModel.ISupportInitialize).BeginInit
         Me.SuspendLayout
         'fileImage
@@ -22,15 +23,29 @@ Public Class ImageViewer
         Me.fileImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         'btnSave
         Me.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK
         Me.btnSave.Location = New System.Drawing.Point(110, 240)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(75, 23)
-        Me.btnSave.Text = "Save..."
+        Me.btnSave.Text = "&Save..."
         Me.btnSave.UseVisualStyleBackColor = true
         AddHandler Me.btnSave.Click, AddressOf Me.btnSave_Click
+        'btnClose
+        Me.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClose.Location = New System.Drawing.Point(-87, -35)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabStop = false
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        AddHandler Me.btnClose.Click, AddressOf Me.btnClose_Click
         'ImageViewer
+        Me.AcceptButton = Me.btnSave
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClose
+        Me.ClientSize = New System.Drawing.Size(284, 263)
+        Me.Controls.Add(Me.btnClose)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.fileImage)
         Me.Name = "ImageViewer"
@@ -39,6 +54,7 @@ Public Class ImageViewer
         CType(Me.fileImage,System.ComponentModel.ISupportInitialize).EndInit
         Me.ResumeLayout(false)
     End Sub
+    Private btnClose As System.Windows.Forms.Button
     Private btnSave As System.Windows.Forms.Button
     Friend fileImage As System.Windows.Forms.PictureBox
     
@@ -53,5 +69,9 @@ Public Class ImageViewer
         If SaveFileDialog.ShowDialog = DialogResult.OK Then
             fileImage.Image.Save(SaveFileDialog.FileName)
         End If
+    End Sub
+    
+    Sub btnClose_Click()
+        Me.Close()
     End Sub
 End Class
